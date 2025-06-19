34a4abfabfabcd82b522990eb4cd7ba6a78f384d
Asrın Kol <kolasrin@gmail.com>
2025-06-19 17:08:38 +0300

    Swagger Authorization system added

--- YTU-test/Program.cs
+++ YTU-test/Program.cs
@@ -1,5 +1,6 @@
 
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using YTU_test.Data;
 using Serilog;
 
@@ -18,19 +19,40 @@ Log.Logger = new LoggerConfiguration()
     .CreateLogger();
 
 builder.Host.UseSerilog();
-
-
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+c.SwaggerDoc("v1", new OpenApiInfo { Title = "YTU API", Version = "v1" });
+
+c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+{
+    Name = "Authorization",
+    Type = SecuritySchemeType.ApiKey,
+    Scheme = "Bearer",
+    In = ParameterLocation.Header,
+    Description = "Enter your JWT token here...  Example ( Bearer bisilerbisiler)"
+});
+
+c.AddSecurityRequirement(new OpenApiSecurityRequirement
+   {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+                new List<string>()
+        }
+   });
+});
 
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
